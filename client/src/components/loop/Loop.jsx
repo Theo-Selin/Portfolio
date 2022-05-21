0c255cc2f8b98e7b3427f37f635b48bae25fc1a0
Theo Selin <theo.selin@hotmail.com>
2022-05-21 12:14:32 +0200

    Loop performance fix attempt

--- client/src/components/loop/Loop.jsx
+++ client/src/components/loop/Loop.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import "./loop.css"
+import React from "react";
+import "./loop.css";
 
 const Loop = () => {
   return (
-    <div className='scrolling__image_container'>
+    <div className="scrolling__image_container">
       <div className="scrolling__image image1"></div>
       <div className="scrolling__image image2"></div>
       <div className="scrolling__image image3"></div>
@@ -14,8 +14,9 @@ const Loop = () => {
       <div className="scrolling__image image8"></div>
       <div className="scrolling__image image9"></div>
       <div className="scrolling__image image10"></div>
+      <div className="scrolling__image image11"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Loop
+export default Loop;
