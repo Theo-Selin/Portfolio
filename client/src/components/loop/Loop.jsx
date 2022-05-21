973b78632d5ccdce96848847ba3d86e2062b4a34
Theo Selin <theo.selin@hotmail.com>
2022-05-21 13:00:16 +0200

    Loop performance test 1

--- client/src/components/loop/Loop.jsx
+++ client/src/components/loop/Loop.jsx
@@ -3,19 +3,21 @@ import "./loop.css";
 
 const Loop = () => {
   return (
-    <div className="scrolling__image_container">
-      <div className="scrolling__image image1"></div>
-      <div className="scrolling__image image2"></div>
-      <div className="scrolling__image image3"></div>
-      <div className="scrolling__image image4"></div>
-      <div className="scrolling__image image5"></div>
-      <div className="scrolling__image image6"></div>
-      <div className="scrolling__image image7"></div>
-      <div className="scrolling__image image8"></div>
-      <div className="scrolling__image image9"></div>
-      <div className="scrolling__image image10"></div>
-      <div className="scrolling__image image11"></div>
-    </div>
+    <>
+      <div className="scrolling__image_container">
+        <div className="scrolling__image image1"></div>
+        <div className="scrolling__image image2"></div>
+        <div className="scrolling__image image3"></div>
+        <div className="scrolling__image image4"></div>
+        <div className="scrolling__image image5"></div>
+        <div className="scrolling__image image6"></div>
+        <div className="scrolling__image image7"></div>
+        <div className="scrolling__image image8"></div>
+        <div className="scrolling__image image9"></div>
+        <div className="scrolling__image image10"></div>
+        <div className="scrolling__image image11"></div>
+      </div>
+    </>
   );
 };
 
