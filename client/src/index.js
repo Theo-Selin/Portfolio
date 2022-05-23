bf9427c0504b97e2d743afd58e063dafe2411f0f
Theo-Selin <theo.selin@hotmail.com>
2022-05-23 13:21:48 +0200

    Scrolling div attempt

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,4 @@ import "./index.css"
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-window.addEventListener("load",function() {
-    setTimeout(function(){
-        // This hides the address bar:
-        window.scrollTo(0, 1);
-    }, 0);
-});
 root.render(<App tab="home" />);
